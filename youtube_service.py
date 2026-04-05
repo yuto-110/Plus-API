f5dce78ba40330239e1587b0053604fa0aa2e13d
yuto-110 <yutoyuto184@gmail.com>
2026-04-05 13:20:07 +0900

    Update youtube_service.py

--- youtube_service.py
+++ youtube_service.py
@@ -22,7 +22,7 @@ BASE_OPTS = {
     "ignore_no_formats_error": True,
     "extractor_args": {
         "youtube": {
-            "player_client": ["tv_embedded"],
+            "player_client": ["mweb"],
         }
     },
 }
